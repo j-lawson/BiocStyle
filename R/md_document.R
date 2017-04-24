55426458a56fb8dc842736133be6f9faafa57cdf
hpages@fhcrc.org <hpages@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2017-04-24 19:45:44 +0000

    Creating branch for BioC 3.5 release
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_5/madman/Rpacks/BiocStyle@129128 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/md_document.R
@@ -0,0 +1,67 @@
+md_document <- function(toc = TRUE, ...) {
+  
+  ## load the package to expose macros
+  require(BiocStyle, quietly = TRUE)
+  
+  base_format = rmarkdown::md_document(toc = toc, md_extensions = "-markdown_in_html_blocks", ...)
+  
+  ## append kramdown toc label at the very beginning of document body
+  if ( isTRUE(toc) ) {
+    base_format$pre_processor <- function(metadata, input_file, runtime, knit_meta, files_dir, output_dir) {
+      lines <- readUTF8(input_file)
+      
+      doc <- rmarkdown:::partition_yaml_front_matter(lines)
+      lines <- c(doc$front_matter, "{:toc}", doc$body)
+      
+      writeUTF8(lines, input_file)
+    }
+  }
+  
+  post_processor <- function(metadata, input_file, output_file, clean, verbose) {
+    lines <- readUTF8(output_file)
+    
+    # move all headers one level down (for proper formatting when embedded in the website)
+    pattern <- '^(#+ )'
+    lines <- gsub(pattern, '#\\1', lines)
+    
+    # make sure there is a newline before headers (pandoc removes them if preceeded by, e.g., <p>...</p>)
+    idx <- grep(pattern, lines)
+    idx <- idx[lines[idx - 1L] != ""]
+    if ((l = length(idx))) {
+      idx <- idx + 0:(l-1L)
+      res <- vector(mode="character", length(lines)+l)
+      res[-idx] <- lines
+      lines <- res
+    }
+    
+    # remove empty line between TOC and kramdown reference name
+    lines <- modifyLines(lines, from="{:toc}", offset=-1L, insert=NULL)
+    
+    header <- NULL
+    
+    # add document title
+    if ( !is.null(metadata$title) )
+      header <- c(sprintf("# %s", metadata$title), "{:.no_toc}", "", header)
+    
+    # add author affiliations
+    if ( !is.null((auth_affil <- auth_affil_html(metadata))) )
+      header <- c(header, auth_affil, "")
+    
+    # TOC header
+    header <- c(header, "## Contents", "{:.no_toc}", "")
+    
+    lines <- c(header, lines)
+    
+    writeUTF8(lines, output_file)
+    output_file
+  }
+  
+  knitr = knitr_options(opts_knit = list(width = 80L))
+  
+  # return format
+  rmarkdown::output_format(knitr = knitr,
+                           pandoc = list(args = "--atx-headers"),
+                           clean_supporting = FALSE,
+                           post_processor = post_processor,
+                           base_format = base_format)
+}
---
+++ R/utils.R
@@ -0,0 +1,61 @@
+modifyLines <- function (lines, from, to, replace = TRUE, before = FALSE, 
+                         offset = 0L, fixed = TRUE, insert = "") {
+  from <- grep(from, lines, fixed=fixed)[1L]
+  
+  # exit if nothing found
+  if (is.na(from)) return(lines)
+  
+  if (missing(to)) {
+    to <- from
+  } else {
+    to <- grep(to, lines, fixed=fixed)
+    if (length(to)==0) return(lines) else to = to[to > from][1L]
+  }
+  
+  offset <- rep_len(offset, 2L)
+  from <- from + offset[1L]
+  to <- to + offset[2L]
+  
+  if (isTRUE(replace)) { # substitute lines from-to
+    c(lines[1:(from-1L)], insert, lines[(to+1L):length(lines)])
+  } else {
+    if (isTRUE(before)) # insert before
+      c(lines[1:(from-1L)], insert, lines[from:length(lines)])
+    else # insert after
+      c(lines[1:to], insert, lines[(to+1L):length(lines)])
+  }
+}
+
+readUTF8 = function(file, ...) readLines(file, encoding = 'UTF-8', warn = FALSE, ...)
+
+writeUTF8 = function(text, ...) writeLines(enc2utf8(text), ..., useBytes = TRUE)
+
+biocTempfile = function(name) {
+  biocDir = file.path(tempdir(), "BiocStyle")
+  if (!dir.exists(biocDir))
+    dir.create(biocDir)
+  
+  file.path(biocDir, name)
+}
+
+sub_ext = function(file, ext) sub("([.][[:alnum:]]+)?$", ext, file)
+
+## recursively merge two lists
+merge_lists <- function (base_list, overlay_list) {
+  if (length(base_list) == 0L)
+    overlay_list
+  else if (length(overlay_list) == 0L)
+    base_list
+  else {
+    merged_list <- base_list
+    for (name in names(overlay_list)) {
+      base <- base_list[[name]]
+      overlay <- overlay_list[[name]]
+      if (is.list(base) && is.list(overlay))
+        merged_list[[name]] <- merge_lists(base, overlay)
+      else 
+        merged_list[[name]] <- overlay_list[[name]]
+    }
+    merged_list
+  }
+}
